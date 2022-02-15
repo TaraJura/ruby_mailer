4b2e5c37044ae1d3eb2611c119c1b0b3f0059a3f
Jiří Novák <jirkanovakneda11@seznam.cz>
2022-02-15 16:00:33 +0100

    working version

---
+++ app/mailers/application_mailer.rb
@@ -0,0 +1,4 @@
+class ApplicationMailer < ActionMailer::Base
+  default from: "jiri.novak@virtualmaster.com"
+  layout "mailer"
+end
---
+++ config/initializers/smtp.rb
@@ -0,0 +1,8 @@
+if Rails.env != 'test'
+  smtp = YAML.safe_load(File.open("#{Rails.root}/config/smtp.yml"))
+  env_smtp = smtp[Rails.env]
+  unless env_smtp.nil?
+    env_smtp.symbolize_keys!
+    ActionMailer::Base.smtp_settings = env_smtp
+  end
+end
